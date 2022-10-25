11051042bdaa6f69699925c77aac27d4bf355f68
Farooq Ansari <farooq.ansari21297@gmail.com>
2022-10-25 14:28:58 +0530

    changed asgi.py

--- server/proj_skrabble/proj_skrabble/asgi.py
+++ server/proj_skrabble/proj_skrabble/asgi.py
@@ -9,8 +9,21 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import core.routing
 from django.core.asgi import get_asgi_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj_skrabble.settings')
 
-application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            core.routing.websocket_urlpatterns
+        )
+    ),
+})
+
