844e913f4b664ba21d27625f5ab9f8c20cd6a0b2
Farooq Ansari <farooq.ansari21297@gmail.com>
2022-10-28 23:15:09 +0530

    word gamelogic optimizations

--- server/proj_skrabble/skrabble-app/src/utils/gameLogic.js
+++ server/proj_skrabble/skrabble-app/src/utils/gameLogic.js
@@ -126,19 +126,18 @@ export async function getWordsEndingOnCursor(cursor, board, wordsMade, activePla
   // console.log("startX", startX, "endX", endX);
 
   // find possible word combinations
-
+  
   for (let sindex = startX; sindex <= endX; sindex++) {
-    for (let eindex = endX; eindex >= sindex; eindex--) {
-      let combOfLetters = Object.create(null);
-      combOfLetters.word = "";
-      combOfLetters.location = [];
-      for (let x = sindex; x <= eindex; x++) {
-        combOfLetters["word"] += board[cursorY][x]["keyVal"];
-        combOfLetters["location"].push([cursorY, x]);
-      }
-      if (combOfLetters["word"].length > 0) {
-        possibleWords.add(JSON.stringify(combOfLetters));
-      }
+    let eindex = endX
+    let combOfLetters = Object.create(null);
+    combOfLetters.word = "";
+    combOfLetters.location = [];
+    for (let x = sindex; x <= eindex; x++) {
+      combOfLetters["word"] += board[cursorY][x]["keyVal"];
+      combOfLetters["location"].push([cursorY, x]);
+    }
+    if (combOfLetters["word"].length > 0) {
+      possibleWords.add(JSON.stringify(combOfLetters));
     }
   }
 
@@ -161,9 +160,11 @@ export async function getWordsEndingOnCursor(cursor, board, wordsMade, activePla
       // console.log("isEnglishWord: " + isEnglishWordReturns)
       // console.log("isWordTaken: " + isWordTakenReturns)
 
-      if (isEnglishWordReturns && !isWordTakenReturns) {
-        // console.log("Word found " + possibleWordObj["word"]);
-        newWordsMadeTemp.add(JSON.stringify(possibleWordObj));
+      if (!isWordTakenReturns) {
+        if (isEnglishWordReturns) {
+          console.log("Word found " + possibleWordObj["word"]);
+          newWordsMadeTemp.add(JSON.stringify(possibleWordObj));
+        }
       }
     }
   };
